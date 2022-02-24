de86d563804f02ac82bdd6323bc21f1bd3b70298
Sergans <gan7er@yandex.ru>
2022-02-24 20:52:17 +0300

    add class

---
+++ MDLesson-1/MDLesson-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MDLesson_1
+{
+   public enum States
+   {
+        Free=0,Booked=1
+   }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
---
+++ MDLesson-1/MDLesson-1/Table.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDLesson_1
+{
+  public  class Table
+    {
+        public int Id { get; set; }
+        public States State { get; set; }
+
+        public int SeatsCount { get; set; }
+        public Table(int id)
+        {
+            Id = id;
+            State = States.Free;
+            
+        }
+    }
+}
