034ce5e84aae27be752993e3e19b535cc58ce93a
Sergans <gan7er@yandex.ru>
2022-02-26 10:49:29 +0300

    add metoth

--- MDLesson-1/MDLesson-1/Program.cs
+++ MDLesson-1/MDLesson-1/Program.cs
@@ -4,5 +4,7 @@ using MDLesson_1;
 Restaurant restaurant = new Restaurant();
 Console.WriteLine();
 
+
+ 
 public enum State { Free=0,Booked=1}
 
--- MDLesson-1/MDLesson-1/Restaurant.cs
+++ MDLesson-1/MDLesson-1/Restaurant.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MDLesson_1
@@ -11,11 +12,31 @@ namespace MDLesson_1
        private readonly List<Table>_tables=new List<Table>();
         public Restaurant()
         {
-            for (ushort i=0; i <=10;i++)
+            for (ushort i=1; i <=10;i++)
             {
               _tables.Add(new Table(i));
             }
         }
+       public void BookFreeTable(int CountOfPersons)
+       {
+            Console.WriteLine("Добрый день! Подождите секунду я подберу для Вас столик и подтвержу Вашу бронь");
+            var table = _tables.FirstOrDefault(t=>t.SeatsCount>CountOfPersons&&t.State==State.Free);
+            Thread.Sleep(1000 * 5);
+
+            Console.WriteLine(table is null ? $"К сожалению столиков нет" : $"Готово, Ваш столик номер{table.Id}");
+       }
+        public void BookFreeTableAsync(int CountOfPersons)
+        {
+            Console.WriteLine("Добрый день! Подождите секунду я подберу для Вас столик. Вам придет уведомление");
+            Task.Run(async () =>
+            {
+                var table = _tables.FirstOrDefault(t => t.SeatsCount > CountOfPersons && t.State == State.Free);
+                await Task.Delay(1000 * 5);
+                table?.SetState(State.Booked);
+
+                Console.WriteLine(table is null ? $"К сожалению столиков нет" : $"Готово, Ваш столик номер{table.Id}");
+            });
+         }  
 
     }
 }
