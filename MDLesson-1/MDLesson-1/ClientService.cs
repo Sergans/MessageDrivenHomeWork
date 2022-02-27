a09ab45c6124ecf6b6d662ad5e29783a44062638
Sergans <gan7er@yandex.ru>
2022-02-27 15:06:53 +0300

    add class

---
+++ MDLesson-1/MDLesson-1/ClientService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDLesson_1
+{
+    public class ClientService
+    {
+      
+    }
+}
